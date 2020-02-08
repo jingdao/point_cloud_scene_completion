9cb8bb251cacfb73ea31b2af563477f6cc8183ed
Chen <jchen490@gatech.edu>
2020-02-08 14:27:40 -0500

    change accuracy metrics

--- get_accuracy.py
+++ get_accuracy.py
@@ -1,6 +1,7 @@
 import numpy
 from util import loadPLY
 import sys
+numpy.random.seed(0)
 
 if len(sys.argv) < 3:
     print("Usage: python get_accuracy.py ground_truth.ply prediction.ply")
@@ -9,18 +10,48 @@ if len(sys.argv) < 3:
 ground_truth_points, _ = loadPLY(sys.argv[1])
 predicted_points, _ = loadPLY(sys.argv[2])
 
-voxel_resolution = 0.001
-ground_truth_voxels = set([tuple(p) for p in numpy.round((ground_truth_points[:,:3] / voxel_resolution)).astype(int)])
-if len(predicted_points) > 0:
-    predicted_voxels = set([tuple(p) for p in numpy.round((predicted_points[:,:3] / voxel_resolution)).astype(int)])
-else:
-    predicted_voxels = set()
+#normalize color to 0->1
+ground_truth_points[:,3:6] /= 255.0
+predicted_points[:,3:6] /= 255.0
+
+#randomly shuffle points to that voxels can be sampled non-deterministically
+numpy.random.shuffle(ground_truth_points)
+numpy.random.shuffle(predicted_points)
+
+voxel_resolution = 0.05
+ground_truth_voxels = {}
+predicted_voxels = {}
+voxel_coordinates = numpy.round((ground_truth_points[:,:3] / voxel_resolution)).astype(int)
+for i in range(len(ground_truth_points)):
+    k = tuple(voxel_coordinates[i])
+    if not k in ground_truth_voxels:
+        ground_truth_voxels[k] = ground_truth_points[i,:]
+      
+voxel_coordinates = numpy.round((predicted_points[:,:3] / voxel_resolution)).astype(int)
+for i in range(len(predicted_points)):
+    k = tuple(voxel_coordinates[i])
+    if not k in predicted_voxels:
+        predicted_voxels[k] = predicted_points[i,:]
     
 print('ground_truth_points', ground_truth_points.shape, len(ground_truth_voxels), 'voxels')
 print('predicted_points', predicted_points.shape, len(predicted_voxels), 'voxels')
 
-common_voxels = ground_truth_voxels.intersection(predicted_voxels)
-accuracy = 1.0 * len(common_voxels) / len(ground_truth_voxels)
-print('accuracy', accuracy)
+common_voxels = set(ground_truth_voxels.keys()).intersection(set(predicted_voxels.keys()))
+voxel_recall = 1.0 * len(common_voxels) / len(ground_truth_voxels) if len(common_voxels)>0 else 0
+voxel_precision = 1.0 * len(common_voxels) / len(predicted_voxels) if len(common_voxels)>0 else 0
+print('voxel_precision', voxel_precision)
+print('voxel_recall', voxel_recall)
+
+position_rmse = 0
+color_rmse = 0
+for k in common_voxels:
+    difference_squared = (ground_truth_voxels[k] - predicted_voxels[k])**2
+    position_rmse += difference_squared[:3].sum()
+    color_rmse += difference_squared[3:6].sum()
+position_rmse = numpy.sqrt(position_rmse / (len(common_voxels)*3))
+color_rmse = numpy.sqrt(color_rmse / (len(common_voxels)*3))
+print('position_rmse', position_rmse)
+print('color_rmse', color_rmse)
+
 
 
--- make_synthetic_data.py
+++ make_synthetic_data.py
@@ -1,5 +1,6 @@
 import numpy
 from util import savePLY
+numpy.random.seed(0)
 
 #create point cloud from wall (XZ plane)
 N = 10000
@@ -13,5 +14,14 @@ savePLY('wall.ply', pcd)
 hole_position = [0.75, 0, 0.75]
 hole_radius = 0.2
 r = numpy.sqrt((pcd[:,0]-hole_position[0])**2 + (pcd[:,2]-hole_position[2])**2)
-pcd = pcd[r > hole_radius]
-savePLY('wall_with_hole.ply', pcd)
+savePLY('wall_with_hole.ply', pcd[r > hole_radius, :])
+
+#fill in hole with color
+pcd_color = pcd.copy()
+pcd_color[r <= hole_radius, 3:6] = [255, 0, 0]
+savePLY('wall_with_color.ply', pcd_color)
+
+#add noise
+pcd_noise = pcd.copy()
+pcd_noise[:,:3] += numpy.random.randn(N,3)*0.01
+savePLY('wall_with_noise.ply', pcd_noise)
