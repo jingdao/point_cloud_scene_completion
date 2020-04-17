8e32cc38807eb2ff6541c4eae8b5547e2e732359
jchen490 <jchen490@gatech.edu>
2020-04-17 17:20:27 -0400

    add branch for mask prediction

--- pix2pix/train.sh
+++ pix2pix/train.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python3 -u pix2pix.py --mode train --output_dir model --lr 0.0002 --batch_size 10 --max_epochs 100 --input_dir train/ --which_direction AtoB 2>/dev/null
+python3 -u pix2pix.py --mode train --output_dir model \
+--lr 0.0001 \
+--batch_size 10 \
+--max_epochs 100 \
+--input_dir train/ \
+--which_direction AtoB 2>/dev/null
